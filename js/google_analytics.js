b8ee1331ce27473dbb27b02767ef63dc2e6fbb37
sunheyi6 <50973219+sunheyi6@users.noreply.github.com>
2023-11-29 09:02:47 +0000

    remove gulp Updated By Github Actions

---
+++ js/google_analytics.js
@@ -0,0 +1,10 @@
+<head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MZXCQSZ8FT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MZXCQSZ8FT');
+</script>
+</head>
